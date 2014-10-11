b16256aae6c1f849855c003e207fb36ebb0dcf6f
Kumar Anirudha <anirudhastark@yahoo.com>
2014-10-11 08:33:21 +0530

    data included

---
+++ data/display.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>User data:</h1>";
+echo "Nos  : ".$_GET['nos']."<br>";
+echo "City From : ".$_GET['cityfrom']."<br>";
+echo "City To : ".$_GET['cityto']."<br>";
+echo "Date From : ".$_GET['datefrom']."<br>";
+echo "Date To : ".$_GET['dateto']."<br>";
+?>
